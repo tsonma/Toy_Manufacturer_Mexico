b6b661ffbec284003f4c61db31b794f7a2584cad
Son Ma <t.son.ma@outlook.com>
2024-11-10 12:35:36 -0500

    ready to publish

--- Theory/2.Setup_Git.py
+++ Theory/2.Setup_Git.py
@@ -16,8 +16,13 @@
 # git commit -m 'first working file of project. You can add any relevant description'
 
 # How to publish to GitHub for the first time
-# git remote add origin 
+# git remote add origin https://github.com/tsonma/Toy_Manufacturer_Mexico.git
 
+# Look at current branch name
+# git branch
+
+# How to rename the local branh
+# git branch -M main
 
 #How to pull data from GitHub
 # git pull origin main
