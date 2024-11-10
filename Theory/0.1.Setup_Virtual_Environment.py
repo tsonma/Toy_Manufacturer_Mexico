fb0d612f03ba32ec5513081344ad1204903d7dac
Son Ma <t.son.ma@outlook.com>
2024-11-10 10:29:40 -0500

    update library

--- Theory/0.1.Setup_Virtual_Environment.py
+++ Theory/0.1.Setup_Virtual_Environment.py
@@ -1,8 +1,8 @@
 # Step 1 Create Virtual Environment in Terminal
-# python -m venv env
+# python -m venv venv
 
 # Step 2 Activate in terminal
-# .\env\Scripts\activate
+# .\venv\Scripts\activate
 
 # Step 3 Deactivate in terminal
 # deactivate
