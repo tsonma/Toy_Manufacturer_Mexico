21fd33b0648202f55e3e0c214953013a608bbfd1
Son Ma <t.son.ma@outlook.com>
2024-11-10 10:22:20 -0500

    first working file of project. You can add any relevant description

---
+++ Theory/0.2.Setup_Git.py
@@ -0,0 +1,28 @@
+# All steps need to be done in the terminal
+
+# Create a folder
+# mkdir folder_name
+
+# How to change directory
+# cd folder_name
+
+# Initialize the git in the folder created
+# git init
+
+# How to add all changes in the staging file. This will ignore the data from .gitignore
+# git add .
+
+# How to commit the changes
+# git commit -m 'first working file of project. You can add any relevant description'
+
+# How to publish to GitHub for the first time
+# git remote add origin https://github.com/user_name/data_cleaning_maven.git
+
+#How to pull data from GitHub
+# git pull origin main
+
+# How to republish to GitHub
+# git push origin main
+
+# How to remove origin if you have errors
+# git remote remove origin
